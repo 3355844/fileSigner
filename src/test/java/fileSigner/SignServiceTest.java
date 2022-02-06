0fb2e802267bd2bd7c39161aaff8ef96109e7a50
3355844 <3355844@gmail.com>
2022-02-06 18:55:51 +0200

    Updated spring-boot-starter-parent to 2.6.3, maven-surefire-plugin to 3.0.0-M5, junit-jupiter-engine to 5.8.2

--- src/test/java/fileSigner/SignServiceTest.java
+++ src/test/java/fileSigner/SignServiceTest.java
@@ -1,7 +1,7 @@
 package fileSigner;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
